1d2aa5fd1e2334c4894e45794b207426c15f76a0
Ereli123 <132412194+Ereli123@users.noreply.github.com>
2023-05-19 21:55:57 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,70 +1,227 @@
+function factorialN() {
+    var N = prompt("Ingrese un valor:", 3);
+    var i = 1;
+    var resp = 1;
 
-function obtenerCantidadDigN () {
-var N parseInt(prompt("Ingrese N", 9478324))
-var cantidad = 0
-while (N != 0){
-    N = parseInt(N / 10)
-    cantida = cantidad + 1
-}
-alert("digitos: " + cantida)
-}
-
-
-function calcularTrapecio () {
-    var h = parseInt(prompt("Ingrese h", 10))
-    var b = parseInt(prompt("Ingrese b", 5))
-    var B = parseInt(prompt("Ingrese B", 15))
-    var a = parseInt(prompt("Ingrese a", 10))
-    var c = parseInt(prompt("Ingrese c", 7))
-    var area = 0
-    var perimetro = 0
-    area = ((b+B) / 2) * h
-    perimetro = a + b + B + c
-    alert("El area es: " + area)
-    alert("El perimetro es: " + perimetro)
-
-}
-
-function determinarNesParImpar(){
-    var N = parseInt(prompt("Ingrese N"))
-    if ((N % 2) == 0) {
-        alert(N + " es par.")
+    if (N >= 0) {
+        if (N == 0) {
+            alert("El factorial de " + N + " es " + resp + ".");
+        } else {
+            while (i <= N) {
+                resp *= i;
+                i += 1;
+            }
+            alert("El factorial de " + N + " es " + resp + ".");
+        }
     } else {
-        alert(N + " es impar.")
+        alert("No existe el factorial de un negativo.");
     }
 }
 
+function mayorAyB() {
+    var A = prompt("Ingrese A", 10);
+    A = parseInt(A);
+    var B = prompt("Ingrese B", 7);
+    B = parseInt(B);
+
+    if (A == B) {
+        alert("Los dos son iguales.");
+    } else {
+        if (A > B) {
+            alert("El mayor es A: " + A);
+        } else {
+            alert("El mayor es B: " + B);
+        }
+    }
+}
+
+function menorAyB() {
+    var A = prompt("Ingrese A", 10);
+    A = parseInt(A);
+    var B = prompt("Ingrese B", 7);
+    B = parseInt(B);
+
+    if (A == B) {
+        alert("Los dos son iguales.");
+    } else {
+        if (A > B) {
+            alert("El menor es B: " + B);
+        } else {
+            alert("El menor es A: " + A);
+        }
+    }
+}
+
+function obtenerDiv() {
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+    var resultado = ~~(A / B);
+
+    alert(resultado);
+}
+
+function obtenerMod() {
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+    var resultado = A % B;
+
+    alert(resultado);
+}
+
+function digitosN() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var resp = N;
+    var cantidad = 1;
+
+    if (N == 0) {
+        cantidad = 1;
+        alert(cantidad + " dígitos");
+    } else {
+        while (N != 0) {
+            N = parseInt(N / 10);
+            cantidad += 1;
+        }
+        alert(cantidad + " dígitos");
+    }
+}
+
+function retornarDigitosN() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    if (N == 0) {
+        dig = N;
+        alert(dig);
+    } else {
+        while (N != 0) {
+            dig = N % 10;
+            alert(dig);
+            N = parseInt(N / 10);
+        }
+    }
+}
+
+function mostrarTres() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    while (N != 0) {
+        dig = N % 10;
+        if (dig == 3) {
+            alert("Existe el número 3");
+            break
+        }
+        N = parseInt(N / 10);
+    }
+}
+
+function areaPerimetroTrapecio() {
+    var h = parseInt(prompt("Ingrese h:", 10));
+    var b = parseInt(prompt("Ingrese b:", 5));
+    var B = parseInt(prompt("Ingrese B:", 15));
+    var a = parseInt(prompt("Ingrese a:", 10));
+    var c = parseInt(prompt("Ingrese c:", 7));
+
+    var Area = 0, Perimetro = 0;
+
+    Area = ((b + B) / 2) * h;
+    Perimetro = a + b + B + c;
+
+    alert("El área es: " + Area);
+    alert("El perímetro es: " + Perimetro);
+}
+
+function NParOImpar() {
+    /* If normal
+
+    var N = parseInt(prompt("Ingrese N:", 115));
+
+    if ((N % 2) == 0) {
+        alert(N + " es par.");
+    } else {
+        alert(N + " es impar.");
+    }
+
+    */
+   
+    // If abreviado
+
+    var N = parseInt(prompt("Ingrese N:", 115));
+    (N % 2) == 0 ? 10 : 1;
+}
+
 function ifNormal() {
-    var N = parseInt(prompt("Ingrese N"))
+    var N = parseInt(prompt("Ingrese N:", 115));
+
     if (N > 5) {
-        N = 10
-    }else {
-        N = 1
+        N = 10;
+    } else {
+        N = 1;
     }
     alert(N)
 }
 
 function ifAbreviado() {
-    var N = parseInt(prompt("Igrese N"))
-    N = (N > 5) ? 10 : 1
-    alert(N)
+    var N = parseInt(prompt("Ingrese N:", 115));
+    N = (N > 5) ? 10 : 1;
+    alert(N);
 }
 
-/*
-var N parseInt(prompt("Ingrese N", 9478324))
-var cantidad = 0
-while (N != 0){
-    N = parseInt(N / 10)
-    cantida = cantidad + 1
-}
-alert("digitos: " + cantida)
-*/
+function digitosNFOR() {
+    var N = parseInt(prompt("Ingresar N", 3));
 
-function obtenerCantidadDigN_FOR(){
-    var N parseInt(prompt("Ingrese N", 9478324))
-var cantidad = 0
-for (cantidad = 0; N != 0; cantidad++) {
-    N = parseInt(N / 10)
+    if (N == 0) {
+        alert(1 + " dígitos");
+    } else {
+        for (var cantidad = 0; N != 0; cantidad += 1) {
+            N = parseInt(N / 10);
+        }
+        alert(cantidad + " dígitos");
+    }
 }
-alert("digitos: " + cantida )
+
+
+
+
+
+function mostrarSuma1_10() {
+    sum = 0
+    ini = 0
+    lim = 0
+    while (ini <= 10) {
+        sum = sum + ini
+        ini = ini + 1
+    }
+    alert("sumatoria de 1 a 10: " + sum)
+}
+
+function calcularAreaVolumenDeCilindro() {
+    var R = parseInt(prompt("Ingrese el radio: ", 3))
+    var H = parseInt(prompt("ingrese la altura: ", 5))
+var area = 2 * Math.PI * R * (R + H)
+var volumen = Math.PI * Math.pow(R, 2) * H
+alert("area: " + area + " volumen: " + volumen)
+}
+
+function NumeroPositivoNegativoNuetro() {
+    var N = parseInt(prompt("Ingrese un numero: ", 1))
+    if (N > 0) {
+        alert( N + " es positivo");
+    }else {
+        if (N < 0){
+            alert( N + " es negativo");
+        }else {
+            alert( N + " es neutro");
+        }
+    }
+}
+
+function MostrarNumeroMenor() {
+    var A = parseInt(prompt("Ingrese un numero: ", 0))
+    var B = parseInt(prompt("Ingrese un numero: ", 1))
+    if ( A < B) {
+        alert( A + " es menor");
+    }else {
+        alert( B + " es menor");
+    }
 }
